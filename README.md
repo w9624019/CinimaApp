eb965c4842f2d121735484c73f704ada7f4ad1a5
Anusha <w9624019@live.tees.ac.uk>
2024-11-11 06:45:21 +0000

    Remove the version

--- README.md
+++
@@ -1,2 +0,0 @@
-# CinimaApp
-App that have list of movies and can save the favourites
--- app/src/main/java/uk/ac/tees/mad/w9624019/cinimaapp/data/User.kt
+++
@@ -1,9 +0,0 @@
-package uk.ac.tees.mad.w9624019.cinimaapp.data
-
-data class User(val fname: String, val sname: String, val isLoggedIn: Boolean = false){
-    companion object {
-        fun createFakeUser() : User {
-            return User("Fake", "User", true)
-        }
-    }
-}
